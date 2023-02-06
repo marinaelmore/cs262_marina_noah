99cf759af746e22a4c346d3898e4474cb22cae97
Marina Elmore <melmore@mba2023.hbs.edu>
2023-02-06 15:11:05 -0500

    unit tests

---
+++ wire_protocol/unit_test.py
@@ -0,0 +1,62 @@
+import unittest
+from memory_manager import User, MemoryManager
+#from client import get_alphanumeric_input
+#from server import *
+from receiver_thread import ReceiverThread
+from server_thread import ServerThread
+
+
+class WireProtocolTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.username = "marina"
+        self.username1 = "noah"
+        self.message = "hello from the other side"
+        self.user = User(self.username)
+        self.memory_manager = MemoryManager()
+
+    #Memory Manager - User Class
+    def test_User_class(self):
+        
+        #Test creation
+        self.assertEqual(self.user.username, self.username)
+        self.assertEqual([], self.user.messages)
+
+        # Test add message
+        msg_list = []
+        msg_list.append(self.message)
+        self.user.add_message(self.message)
+        self.assertEqual(msg_list, self.user.messages)
+
+
+    def test_MemoryManager_class(self):
+        
+        #Test Creation
+        self.assertEqual({},self.memory_manager.users)
+
+        # Test Create User
+        self.memory_manager.create_user(self.username)
+        self.memory_manager.create_user(self.username1)
+        self.assertEqual(2, len(self.memory_manager.users))
+        created_users = list(self.memory_manager.users.keys())
+        self.assertEqual(["marina", "noah"], created_users)
+
+
+        # Test Send Message
+
+        self.memory_manager.send_message(self.username, self.username1)
+
+
+
+        #Test Get Message
+
+
+        #Test List Users
+
+
+        # Test Delete User
+
+
+
+if __name__ == '__main__':
+    unittest.main()
