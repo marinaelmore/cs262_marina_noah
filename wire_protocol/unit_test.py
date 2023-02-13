0b85a1834b3765f4fa291b0f6e530c5d90ca6787
Marina Elmore <melmore@mba2023.hbs.edu>
2023-02-13 15:41:36 -0500

    unit testing

--- wire_protocol/unit_test.py
+++ wire_protocol/unit_test.py
@@ -1,16 +1,31 @@
 import unittest
 from helpers.memory_manager import User, MemoryManager
-# from client import get_alphanumeric_input
-# from server import *
-from receiver_thread import ReceiverThread
-from server_thread import ServerThread
+from chatbot.receiver_thread import ReceiverThread
+from chatbot.server_thread import ServerThread
+from unittest.mock import patch
+
+
+class MockThread:
+    def __init__(self, name):
+        self.name = name
+    
+    def start(self):
+        pass
+
+class MockClient:
+    def __init__(self) -> None:
+        pass
+
+    def send(self):
+        print("Sending")
+        pass 
 
 
 class WireProtocolTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.username = "marina"
-        self.username1 = "noah"
+        self.username = "narina"
+        self.username1 = "moah"
         self.message = "hello from the other side"
         self.user = User(self.username)
         self.memory_manager = MemoryManager()
@@ -38,24 +53,25 @@ class WireProtocolTestCase(unittest.TestCase):
         self.memory_manager.create_user(self.username1)
         self.assertEqual(2, len(self.memory_manager.users))
         created_users = list(self.memory_manager.users.keys())
-        self.assertEqual(["marina", "noah"], created_users)
+        self.assertEqual(["narina", "moah"], created_users)
 
-        # Test Send Message (marina -> noah)
-        self.memory_manager.send_message(self.username1, self.message)
-        noahs_messages = self.memory_manager.users.get(self.username1).messages
-        self.assertEqual(1, len(noahs_messages))
-        self.assertEqual(self.message, noahs_messages[0])
+        # Test Send Message (narina -> moah)
+        self.memory_manager.send_message(self.username, self.username1, self.message)
+        moahs_messages = self.memory_manager.users.get(self.username1).messages
+        self.assertEqual(1, len(moahs_messages))
+        message_format = "{}: {}".format(self.username, self.message)
+        self.assertEqual(message_format, moahs_messages[0])
 
         # Test Get Message
         self.assertEqual(
-            self.message, self.memory_manager.get_message(self.username1))
+            message_format, self.memory_manager.get_message(self.username1))
         self.assertIsNone(self.memory_manager.get_message(self.username))
 
         # Test List Users
-        self.assertEqual(["marina", "noah"],
+        self.assertEqual(["narina", "moah"],
                          self.memory_manager.list_users(""))
-        self.assertEqual(["marina"], self.memory_manager.list_users("m"))
-        self.assertEqual(["noah"], self.memory_manager.list_users("n"))
+        self.assertEqual(["narina"], self.memory_manager.list_users("n"))
+        self.assertEqual(["moah"], self.memory_manager.list_users("m"))
         self.assertEqual([], self.memory_manager.list_users("9"))
 
         # Test Delete User
@@ -63,7 +79,22 @@ class WireProtocolTestCase(unittest.TestCase):
         self.memory_manager.delete_user(self.username)
         self.assertEqual(1, len(self.memory_manager.users))
         curr_users = list(self.memory_manager.users.keys())
-        self.assertEqual(["noah"], curr_users)
+        self.assertEqual(["moah"], curr_users)
+
+
+    def test_Chatbot_server_thread(self):
+        with patch('unit_test.MockThread') as mocked_thread:
+            server_thread = ServerThread(mocked_thread)
+
+            self.assertEqual("marina", server_thread.create(self.username))
+
+        
+        self.assertEqual("marina", "marina")
+
+
+    
+    def test_Chatbot_grpc(self):
+        self.assertEqual("marina", "marina")
 
 
 if __name__ == '__main__':
