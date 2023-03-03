d2d2af343d46d95ec43f5afe6b3b6da66c534bf3
Noah Zweben <noahzweben@gmail.com>
2023-03-03 16:50:05 -0500

    added to requirements

--- logical_clocks/virtual_machine.py
+++ logical_clocks/virtual_machine.py
@@ -1,16 +1,3 @@
-# Each model machine will run at a clock rate determined during initialization. You will pick a random number between 1 and 6, and that will be the number of clock ticks per (real world) second for that machine. This means that only that many instructions can be performed by the machine during that time. Each machine will also have a network queue (which is not constrained to the n operations per second) in which it will hold incoming messages. The (virtual) machine should listen on one or more sockets for such messages.
-
-# Each of your virtual machines should connect to each of the other virtual machines so that messages can be passed between them. Doing this is part of initialization, and not constrained to happen at the speed of the internal model clocks. Each virtual machine should also open a file as a log. Finally, each machine should have a logical clock, which should be updated using the rules for logical clocks.
-
-# SET UP
-# Initialize clock rate (rand int)
-# Initialize network queue
-# Connect to other virtual machines, listen on one or more sockets for such messages
-# Open file as a log
-# Initialize logical clock
-
-# On each clock cycle, if there is a message in the message queue for the machine (remember, the queue is not running at the same cycle speed) the virtual machine should take one message off the queue, update the local logical clock, and write in the log that it received a message, the global time (gotten from the system), the length of the message queue, and the logical clock time.
-
 import asyncio
 import random
 import configparser
