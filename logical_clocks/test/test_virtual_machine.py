c48c2c50fd3d8f9fc65342f9bec51ebe01f154c5
Noah Zweben <noahzweben@gmail.com>
2023-03-05 19:42:44 -0500

    added more test

--- logical_clocks/test/test_virtual_machine.py
+++ logical_clocks/test/test_virtual_machine.py
@@ -4,10 +4,30 @@ import os
 from unittest.mock import Mock
 import asyncio
 import pytest
+import json
 
 sys.path.insert(0, os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..')))  # add parent directory to path
-from virtual_machine import VirtualMachine  # nopep8
+import virtual_machine as vm  # nopep8
+
+machine_1 = {
+    "name": "machine 1",
+    "output_path": "output.txt",
+    "host": "localhost",
+    "port": 9000
+}
+machine_2 = {
+    "name": "machine 2",
+    "output_path": "output2.txt",
+    "host": "localhost",
+    "port": 9001
+}
+machine_3 = {
+    "name": "machine 3",
+    "output_path": "output3.txt",
+    "host": "localhost",
+    "port": 9002
+}
 
 
 class TestVirtualMachineTest:
@@ -15,21 +35,21 @@ class TestVirtualMachineTest:
     def setup_class(self):
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
-        self.vm = VirtualMachine(
-            'machine_1', 'output.txt', 'localhost', '9000', '9001', '9002')
+
+        self.vm = vm.VirtualMachine(
+            "machine_1", machine_1, machine_2, machine_3)
 
     @classmethod
     def teardown_class(self):
         self.loop.close()
 
     def test_init(self):
-        assert (self.vm.machine_id == 'machine_1')
-        assert (self.vm.host == 'localhost')
-        assert (self.vm.my_port == '9000')
-        assert (self.vm.machine_2_port == '9001')
-        assert (self.vm.machine_3_port == '9002')
+        assert self.vm.my_machine == machine_1
+        assert self.vm.machine_2 == machine_2
+        assert self.vm.machine_3 == machine_3
         assert (self.vm.logical_clock == 0)
         assert (self.vm.queue.qsize() == 0)
+        assert self.vm.clock_rate > .15 and self.vm.clock_rate <= 1
         # check that self.ouput_file is created
         assert (os.path.exists('output.txt'))
 
@@ -55,7 +75,7 @@ class TestVirtualMachineTest:
 
         # assert start_server is called with correct args
         mock_start_server.assert_called_once_with(
-            self.vm.queue_protocol, self.vm.host, self.vm.my_port)
+            self.vm.queue_protocol, self.vm.my_machine["host"], self.vm.my_machine["port"])
 
         server_mock.serve_forever.assert_called_once()
 
@@ -70,14 +90,92 @@ class TestVirtualMachineTest:
 
         # assert open_connection is called with correct args
         mock_open_connection.assert_has_calls([
-            mocker.call(self.vm.host, self.vm.machine_2_port),
-            mocker.call(self.vm.host, self.vm.machine_3_port)
+            mocker.call(self.vm.machine_2["host"], self.vm.machine_2["port"]),
+            mocker.call(self.vm.machine_2["host"], self.vm.machine_3["port"])
         ])
 
-        assert self.vm.reader2 == reader_mock
-        assert self.vm.stream2 == stream_mock
-        assert self.vm.reader3 == reader_mock
-        assert self.vm.stream3 == stream_mock
+        assert self.vm.machine_2["reader"] == reader_mock
+        assert self.vm.machine_2["stream"] == stream_mock
+        assert self.vm.machine_3["reader"] == reader_mock
+        assert self.vm.machine_3["stream"] == stream_mock
+
+    @pytest.mark.asyncio
+    async def test_send_clock_time(self, mocker):
+        stream_mock = Mock(spec=asyncio.StreamWriter)
+        mock_write = mocker.patch.object(stream_mock, 'write')
+        self.vm.logical_clock = 627
+        await self.vm.send_clock_time(stream_mock)
+        mock_write.assert_called_once_with(b'627\n')
+
+    @pytest.mark.asyncio
+    async def test_tick_queue_empty(self, mocker):
+        stream_mock_2 = Mock(spec=asyncio.StreamWriter)
+        stream_mock_3 = Mock(spec=asyncio.StreamWriter)
+        self.vm.machine_2["stream"] = stream_mock_2
+        self.vm.machine_3["stream"] = stream_mock_3
+        self.vm.logical_clock = 0
+        mock_send_clock_time = mocker.patch.object(self.vm, "send_clock_time")
+        mock_file_write = mocker.patch.object(self.vm.output_file, "write")
+
+        await self.vm.tick_queue_empty(1)
+        mock_send_clock_time.assert_called_once_with(stream_mock_2)
+        assert self.vm.logical_clock == 1
+
+        await self.vm.tick_queue_empty(2)
+        # assert called multiple times
+        assert mock_send_clock_time.call_args_list[1] == mocker.call(
+            stream_mock_3)
+        assert self.vm.logical_clock == 2
+
+        await self.vm.tick_queue_empty(3)
+        assert self.vm.logical_clock == 3
+        assert mock_send_clock_time.call_args_list[2] == mocker.call(
+            stream_mock_2)
+        assert mock_send_clock_time.call_args_list[3] == mocker.call(
+            stream_mock_3)
+        assert mock_send_clock_time.call_count == 4
+
+        # make sure internal ievent only and call count not called
+        await self.vm.tick_queue_empty(4)
+        assert self.vm.logical_clock == 4
+        assert mock_send_clock_time.call_count == 4
+        assert mock_file_write.call_args_list[3] == mocker.call(
+            "internal, time 3\n")
+
+        assert mock_file_write.call_count == 4
+
+    @pytest.mark.asyncio
+    async def test_tick_queue_has_item(self, mocker):
+        self.vm.logical_clock = 0
+        self.vm.queue.put_nowait(5)
+        # picks greater of 2 times and increments by one
+        mock_file_write = mocker.patch.object(self.vm.output_file, "write")
+        self.vm.tick_queue_has_item()
+        assert self.vm.logical_clock == 6
+        assert mock_file_write.call_count == 1
+
+        self.vm.logical_clock = 10
+        self.vm.queue.put_nowait(8)
+        self.vm.tick_queue_has_item()
+        assert self.vm.logical_clock == 11
+        assert mock_file_write.call_count == 2
+
+
+def test_main(mocker):
+    # assert that whichever id is passed as the vm's identify, machine_2 and 3 are set accordingly
+
+    mocker_loop = mocker.patch.object(asyncio, "get_event_loop")
+    mocker_loop.create_task.side_effect = None
+    mocker_loop.run_forever.side_effect = None
+    json_data = open('config.json').read()
+    config = json.loads(json_data)
+
+    vm_mock = mocker.patch.object(
+        vm.VirtualMachine, "__init__", return_value=None)
+
+    vm.main("machine_2")
+    vm_mock.assert_called_once_with(
+        "machine_2", config["machine_2"], config["machine_1"], config["machine_3"])
 
 
 if __name__ == '__main__':
--- logical_clocks/virtual_machine.py
+++ logical_clocks/virtual_machine.py
@@ -2,25 +2,25 @@ import asyncio
 import random
 import configparser
 import datetime
+import json
 
 
 class VirtualMachine():
 
-    def __init__(self, machine_id, output_log_path, host, myport, port2, port3):
+    def __init__(self, machine_id, my_machine, machine_2, machine_3):
         # Initialize vars
-        self.machine_id = machine_id
+        print("ineher", my_machine)
         self.clock_rate = 1. / random.randrange(1, 7)
-        self.output_file = open(output_log_path, "w")
+        self.output_file = open(my_machine['output_path'], "w")
+        self.machine_id = machine_id
+        self.my_machine = my_machine
+        self.machine_2 = machine_2
+        self.machine_3 = machine_3
         self.logical_clock = 0
-        self.host = host
-        self.my_port = myport
         self.queue = asyncio.Queue()
-        self.machine_2_port = port2
-        self.machine_3_port = port3
 
     async def queue_protocol(self, reader, writer):
         while True:
-
             request = await reader.readline()
             try:
                 self.queue.put_nowait(int(request.decode()))
@@ -31,16 +31,17 @@ class VirtualMachine():
         writer.close()
 
     async def start_vm_server(self):
-        print("Starting server task on port {}".format(self.my_port))
-        start_server_task = await asyncio.start_server(self.queue_protocol, self.host, self.my_port)
+        print("Starting server task on port {}".format(
+            self.my_machine["port"]))
+        start_server_task = await asyncio.start_server(self.queue_protocol, self.my_machine["host"], self.my_machine["port"])
         await start_server_task.serve_forever()
 
     async def connect_to_other_machines(self):
         first_attempt = True
         while True:
             try:
-                self.reader2, self.stream2 = await asyncio.open_connection(self.host, self.machine_2_port)
-                self.reader3, self.stream3 = await asyncio.open_connection(self.host, self.machine_3_port)
+                self.machine_2["reader"], self.machine_2["stream"] = await asyncio.open_connection(self.machine_2["host"], self.machine_2["port"])
+                self.machine_3["reader"], self.machine_3["stream"] = await asyncio.open_connection(self.machine_3["host"], self.machine_3["port"])
                 print()
                 break
             except ConnectionRefusedError:
@@ -53,16 +54,10 @@ class VirtualMachine():
                     print(".", end="", flush=True)
                 await asyncio.sleep(1)
 
-    async def send_clock_time(self, port):
+    async def send_clock_time(self, writer):
         message = f"{self.logical_clock}\n"
-
-        if port == self.machine_2_port:
-            self.stream2.write(message.encode())
-            await self.stream2.drain()
-
-        if port == self.machine_3_port:
-            self.stream3.write(message.encode())
-            await self.stream3.drain()
+        writer.write(message.encode())
+        await writer.drain()
 
     async def run_vm_client(self):
         self.output_file.write("Starting VM\n")
@@ -80,75 +75,65 @@ class VirtualMachine():
             print("\n\n{} has slept for {} seconds".format(
                 self.machine_id, self.clock_rate))
 
-            # print queue length
-
             if self.queue.empty():
                 print("Queue Empty, Roll the Dice")
-                randint = random.randrange(1, 10)
-                ports = []
-                if randint == 1:
-                    ports = [self.machine_2_port]
-                elif randint == 2:
-                    ports = [self.machine_3_port]
-                elif randint == 3:
-                    ports = [self.machine_2_port, self.machine_3_port]
-                else:
-                    # if the value is other than 1-3, treat the cycle as an internal event; update the local logical clock, and log the internal event, the system time, and the logical clock value.
-                    log_msg = f"internal, time {self.logical_clock}\n"
-                    print(log_msg)
-                    self.output_file.write(log_msg)
-
-                if len(ports) > 0:
-                    log_msg = f"send {ports}, time {self.logical_clock}\n"
-                    print(log_msg)
-                    self.output_file.write(log_msg)
-                    for port in ports:
-                        await self.send_clock_time(port)
-
-                self.logical_clock += 1
-
+                dice_roll = random.randrange(1, 10)
+                await self.tick_queue_empty(dice_roll)
             else:
-                print("Queue Length: {}. Printing Message".format(
-                    self.queue.qsize()))
-                msg = self.queue.get_nowait()
-                print("--> Message: {}\n".format(msg))
-                self.logical_clock = max(self.logical_clock, msg) + 1
-
-                # update local logical clock
-                log_msg = f"receive, global time {datetime.datetime.now()}, new time {self.logical_clock}, queue length {self.queue.qsize()}\n"
-                print(log_msg)
-                self.output_file.write(log_msg)
-                self.queue.task_done()
-
-            self.output_file.flush()
+                self.tick_queue_has_item()
             print('This round is finished, night night\n\n')
+
+    async def tick_queue_empty(self, dice_roll):
+        machines = []
+        if dice_roll == 1:
+            machines = [self.machine_2]
+        elif dice_roll == 2:
+            machines = [self.machine_3]
+        elif dice_roll == 3:
+            machines = [self.machine_2, self.machine_3]
+        else:
+            # if the value is other than 1-3, treat the cycle as an internal event; update the local logical clock, and log the internal event, the system time, and the logical clock value.
+            log_msg = f"internal, time {self.logical_clock}\n"
+            print(log_msg)
+            self.output_file.write(log_msg)
+
+        if len(machines) > 0:
+            names = [m["name"] for m in machines]
+            log_msg = f"send {names}, time {self.logical_clock}\n"
+            print(log_msg)
+            self.output_file.write(log_msg)
+            for machine in machines:
+                await self.send_clock_time(machine["stream"])
+
+        self.logical_clock += 1
+        self.output_file.flush()
+
+    def tick_queue_has_item(self):
+        print("Queue Length: {}. Printing Message".format(
+            self.queue.qsize()))
+        msg = self.queue.get_nowait()
+        print("--> Message: {}\n".format(msg))
+        self.logical_clock = max(self.logical_clock, msg) + 1
+
+        # update local logical clock
+        log_msg = f"receive, global time {datetime.datetime.now()}, new time {self.logical_clock}, queue length {self.queue.qsize()}\n"
+        print(log_msg)
+        self.output_file.write(log_msg)
+        self.queue.task_done()
+        self.output_file.flush()
 
 
 def main(machine_id):
     # Parse configurations
+    json_data = open('config.json').read()
+    config = json.loads(json_data)
 
-    config = configparser.ConfigParser()
-    config.sections()
-    config.read('config.ini')
-
-    if machine_id == "machine_1":
-        myhost = config['machine_1']['host']
-        myport = config['machine_1']['port']
-        m2port = config['machine_2']['port']
-        m3port = config['machine_3']['port']
-        output_path = config['machine_1']['output_path']
-    elif machine_id == "machine_2":
-        myhost = config['machine_2']['host']
-        myport = config['machine_2']['port']
-        m2port = config['machine_1']['port']
-        m3port = config['machine_3']['port']
-        output_path = config['machine_2']['output_path']
-    elif machine_id == "machine_3":
-        myhost = config['machine_3']['host']
-        myport = config['machine_3']['port']
-        m2port = config['machine_1']['port']
-        m3port = config['machine_2']['port']
-        output_path = config['machine_3']['output_path']
+    if machine_id in config.keys():
+        my_machine = config[machine_id]
+        # set machine 2 and 3 to the other 2 machine ids
+        other_machines = [x for x in config.keys() if x != machine_id]
+        machine_2 = config[other_machines[0]]
+        machine_3 = config[other_machines[1]]
     else:
         print("Machine name does not exist")
         return None
@@ -156,8 +141,7 @@ def main(machine_id):
     # Start Loop
     loop = asyncio.get_event_loop()
 
-    vm = VirtualMachine(machine_id, output_path,
-                        myhost, myport, m2port, m3port)
+    vm = VirtualMachine(machine_id, my_machine, machine_2, machine_3)
 
     loop.create_task(vm.start_vm_server())
     loop.create_task(vm.run_vm_client())
