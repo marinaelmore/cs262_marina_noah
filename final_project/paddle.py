1700a76aa293bef1fec08a21313c50a3c117bcf5
Marina Elmore <melmore@mba2023.hbs.edu>
2023-04-17 22:06:17 -0400

    play_game.py

---
+++ final_project/paddle.py
@@ -0,0 +1,5 @@
+import pygame as pg
+
+class Paddle():
+    def __init__(self) -> None:
+        pass
--- final_project/play_game.py
+++ final_project/play_game.py
@@ -1,6 +1,12 @@
 import pygame as pg
 from pygame.locals import *
 from config import *
+from random import randint
+
+
+class PlayPong():
+    def __init__(self) -> None:
+        pass
 
 RED = (255, 0, 0)
 BLUE = (51, 153, 255)
@@ -16,11 +22,15 @@ paddle2 = Rect(WINDOW_WIDTH-PADDLE_WIDTH*2, WINDOW_HEIGHT/2, PADDLE_WIDTH, PADDL
 dir = {K_LEFT: (0, 0), K_RIGHT: (0, 0), K_UP: (0, -PADDLE_WIDTH), K_DOWN: (0, PADDLE_WIDTH)}
 
 font = pg.font.SysFont(None, 24)
-img = font.render('hello', True, BLUE)
 
 running = True
 
 while running:
+    player_1_score = 0
+    player_2_score = 0
+    player_1_score_img = font.render('Player 1: {}'.format(player_1_score), True, BLUE)
+    player_2_score_img = font.render('Player 2: {}'.format(player_2_score), True, BLUE)
+
     for event in pg.event.get():
         if event.type == QUIT:
             running = False
@@ -29,9 +39,10 @@ while running:
             if event.key in dir:
                 v = dir[event.key]
                 paddle1.move_ip(v)
-
+    
     screen.fill(BLACK)
-    screen.blit(img, (20, 20))
+    screen.blit(player_1_score_img, (20, 20))
+    screen.blit(player_2_score_img, (WINDOW_WIDTH-100, 20))
     pg.draw.rect(screen, BLUE, paddle1, PADDLE_WIDTH)
     pg.draw.rect(screen, BLUE, paddle2, PADDLE_WIDTH)
     pg.display.flip()
